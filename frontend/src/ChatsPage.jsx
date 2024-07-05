e92c22049e27d722d795788f57ca9e689816ba13
MohomedAadil <aadil.mohomed786@gmail.com>
2024-07-05 18:06:36 +0530

    integrating react chat engine

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
 const ChatsPage = () => {
-    return <>chats...</>
+    const chatProps = useMultiChatLogic(
+        '8a1c4b09-025d-4cc1-aea0-472d88c178f0',
+        props.user.username,
+        props.user.secret
+    );
+    return (
+        <div style={{height: '100vh'}}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{height: '100%'}}/>
+        </div>
+    )
 }
 
 export default ChatsPage
